a27b26183254a81bf97319d415749237cd30671a
SonyVansha <sonyvansha@gmail.com>
2023-12-10 15:54:20 +0700

    fiks error in server.js

--- app/server.js
+++ app/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/honai_star.png"));
+  let img = fs.readFileSync(path.join(__dirname, "images/honkai_star.png"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
